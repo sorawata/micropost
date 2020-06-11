b8eed734d643403bbbedabafc81c8cdfdd9c40d1
sora.watanabe <sorawsora02@gmail.com>
2020-06-11 12:23:47 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -8,12 +8,12 @@ class MicropostsController extends Controller
 {
      public function index()
     {
-        $data = [];
-        if (\Auth::check()) { 
-            
+       $data = [];
+        if (\Auth::check()) {
+            // 認証済みユーザ（閲覧者）を取得
             $user = \Auth::user();
-            
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            // ユーザとフォロー中ユーザの投稿の一覧を作成日時の降順で取得
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
                 'user' => $user,
@@ -21,7 +21,7 @@ class MicropostsController extends Controller
             ];
         }
 
-        
+        // Welcomeビューでそれらを表示
         return view('welcome', $data);
     }
      public function store(Request $request)
